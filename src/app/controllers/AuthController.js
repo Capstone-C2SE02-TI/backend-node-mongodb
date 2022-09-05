e7f8a77ce5ec7cbdf4a68d4abc29f7b142de0ae8
huynhdthanhtuan <tuan40655@gmail.com>
2022-09-05 20:23:26 +0700

    TASK-2: Done User Signup API

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -1,3 +1,4 @@
+const firebase = require("firebase-admin");
 const { validateUserSignUpBody } = require("../../validators");
 const { cryptPassword, comparePassword } = require("../../helpers");
 const {
@@ -26,13 +27,29 @@ function AuthController() {
         }
 
         // Encode password and create new user in DB
+        const currentTimestamp = firebase.firestore.Timestamp.now();
         cryptPassword(password, async function (err, hashPassword) {
-            const newUser = { username, email, phoneNumber, hashPassword };
-            await createNewUser(newUser);
-        });
+            const newUser = {
+                username,
+                email,
+                phoneNumber,
+                hashPassword,
+                createdDate: currentTimestamp,
+                updatedDate: currentTimestamp,
+            };
 
-        // res.status(400).json({ message: "Sign up failed" });
-        res.status(200).json({ message: "Sign up successfully" });
+            await createNewUser(newUser)
+                .then(function () {
+                    return res
+                        .status(200)
+                        .json({ message: "Sign up successfully" });
+                })
+                .catch(function (error) {
+                    return res.status(401).json({
+                        message: "Sign up failed with error: " + error,
+                    });
+                });
+        });
     };
 
     // [POST] /signin
