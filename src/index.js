6e014b002e26e747f26ad0cd9d47e3c40734ddbc
huynhdthanhtuan <tuan40655@gmail.com>
2022-09-05 23:59:24 +0700

    TASK-4: Uninstall not use libraries, prepare for config relative paths

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-// const bodyParser = require("body-parser");
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 const routing = require("./routes");
