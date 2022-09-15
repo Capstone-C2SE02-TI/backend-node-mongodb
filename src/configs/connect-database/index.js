26e12e65775f87ebacb9dcb781de7205543d1440
huynhdthanhtuan <tuan40655@gmail.com>
2022-09-15 22:38:24 +0700

    TASK-10: Ready for test this API

--- src/configs/connect-database/index.js
+++ src/configs/connect-database/index.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require("./service-account-config.json");
+const serviceAccount = require("./service-account.json");
 
 initializeApp({
     credential: cert(serviceAccount),
